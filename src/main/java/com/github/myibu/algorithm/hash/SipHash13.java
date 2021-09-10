745b2ed1548919dee6976a9b6053932119658329
FollowH <1337078409@qq.com>
2021-09-10 17:14:41 +0800

    generate default hash seed for siphash

--- src/main/java/com/github/myibu/algorithm/hash/SipHash13.java
+++ src/main/java/com/github/myibu/algorithm/hash/SipHash13.java
@@ -6,6 +6,12 @@ package com.github.myibu.algorithm.hash;
  * Created on 2021/9/8
  */
 public class SipHash13 extends SipHash {
+    public SipHash13() {
+        super();
+        this.c = 1;
+        this.d = 3;
+    }
+
     public SipHash13(byte[] seed) {
         super(seed, 1, 3);
     }
